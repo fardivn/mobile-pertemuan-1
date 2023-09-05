6cefa4ecbd2efb48d263b6da01d234e3e81d7fd7
fardivn <farahdivanabila5@gmail.com>
2023-09-05 15:38:22 +0700

    mengubah page title

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:
+          const MyHomePage(title: 'Flutter Demo Home Page (Kelompok 5 SIB-3D)'),
     );
   }
 }
